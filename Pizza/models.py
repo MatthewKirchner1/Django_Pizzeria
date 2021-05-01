3717a7b9580a6b7ff2f49a10e1c26214fdab69c2
Matthew Kirchner <Matthew_Kirchner1@baylor.edu>
2021-05-01 14:35:49 -0500

    Fix Comment

--- Pizza/models.py
+++ Pizza/models.py
@@ -17,3 +17,12 @@ class Topping(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.text
